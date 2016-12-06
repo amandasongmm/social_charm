3b15b83551f8a55d86ac889ed17dab2aa7ca435e
amandasongmm <feijuejuanling@gmail.com>
2016-12-06 00:38:52 -0800

    add django project

---
+++ fblogin/alpha/models.py
@@ -0,0 +1,97 @@
+from __future__ import unicode_literals
+from allauth.socialaccount.models import SocialAccount
+import hashlib
+
+
+
+from django.contrib.auth.models import User
+from django.db import models
+from allauth.account.models import EmailAddress
+
+from django.utils import timezone
+
+try:
+    from django.utils.encoding import force_text
+except ImportError:
+    from django.utils.encoding import force_unicode as force_text
+# Create your models here.
+
+
+def profile_image_url(self):
+    fb_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='facebook')
+
+    if len(fb_uid):
+        return "http://graph.facebook.com/{}/picture?width=40&height=40".format(fb_uid[0].uid)
+
+    return "http://www.gravatar.com/avatar/{}?s=40".format(hashlib.md5(self.user.email).hexdigest())
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, primary_key=True, verbose_name='user', related_name='profile')
+    avatar_url = models.CharField(max_length=256, blank=True, null=True)
+
+    def __str__(self):
+        return force_text(self.user.email)
+
+    class Meta():
+        db_table = 'user_profile'
+
+    def __unicode__(self):
+        return "{}'s profile".format(self.user.username)
+
+    class Meta:
+        db_table = 'user_profile'
+
+    def account_verified(self):
+        if self.user.is_authenticated:
+            result = EmailAddress.objects.filter(email=self.user.email)
+            if len(result):
+                return result[0].verified
+        return False
+
+    def profile_image_url(self):
+        fb_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='facebook')
+
+        if len(fb_uid):
+            return "http://graph.facebook.com/{}/picture?width=40&height=40".format(fb_uid[0].uid)
+
+        # url = "http://graph.facebook.com/%s/picture?type=large" % response['id']
+        # avatar = urlopen(url)
+        # profile = user.get_profile()
+        # profile.profile_photo.save(slugify(user.username + " social") + '.jpg',
+        #                         ContentFile(avatar.read()))
+        # profile.save()
+        #
+        # {{user.socialaccount_set.all.0.get_avatar_url}}
+        # {{user.socialaccount_set.all.0.uid}}
+
+        return "http://www.gravatar.com/avatar/{}?s=40".format(hashlib.md5(self.user.email).hexdigest())
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')  # a link to another model.
+    title = models.CharField(max_length=200)  # define text with a limited number of characters
+    text = models.TextField()  # long text without a limit. sounds ideal for blog post content
+    created_date = models.DateTimeField(default=timezone.now)  # date and time
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
---
+++ fblogin/alpha/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+
+from django.template import loader
+
+from django.template.context import RequestContext
+
+from django.views.generic import TemplateView
+# Create your views here.
+
+
+def index(request):
+    template = loader.get_template('alpha/index.html')
+    return HttpResponse(template.render(request))
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+def profile_photo_page(request, user_id_name):
+    return HttpResponse("Hello, %s" % user_id_name)
+
+
+# def home(request):
+#     context = RequestContext(request,
+#                              {'user': request.user})
+#     return render_to_response('templates/alpha/home.html',
+#                               context_instance=context)
+
+
+class HomePageView(TemplateView):
+    # def get(self, request, **kwargs):
+    #     return render(request, 'alpha/index.html', context=None)
+    template_name = 'alpha/index.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'alpha/about.html'
